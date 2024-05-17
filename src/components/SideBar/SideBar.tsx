5b2f7430ed7396788755e0d475635edc99e7484c
Andrey Bakin <andybulkin@yandex.ru>
2024-05-18 00:54:58 +0300

    Созданы компоненты Player, SideBar, CenterBlock, Signin, Signup

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import styles from "./UserSideBar.module.css";
+
+export default function SideBar() {
+  return (
+    <div className={styles.sidebarMain}>
+      <div className={styles.sidebarPersonal}>
+        <p className={styles.sidebarPersonalName}>Sergey.Ivanov</p>
+        <div className={styles.sidebarIcon}>
+          <svg>
+            <use xlinkHref="img/icon/sprite.svg#logout" />
+          </svg>
+        </div>
+      </div>
+      <div className={styles.sidebarBlock}>
+        <div className={styles.sidebarList}>
+          <div className={styles.sidebarItem}>
+            <a className={styles.sidebarLink} href="#">
+              <Image
+                className={styles.sidebarImg}
+                src="/img/playlist01.png"
+                alt="Плейлист дня"
+                width={250}
+                height={150}
+              />
+            </a>
+          </div>
+          <div className={styles.sidebarItem}>
+            <a className={styles.sidebarLink} href="#">
+              <Image
+                className={styles.sidebarImg}
+                src="/img/playlist02.png"
+                alt="100 танцевальных хитов"
+                width={250}
+                height={150}
+              />
+            </a>
+          </div>
+          <div className={styles.sidebarItem}>
+            <a className={styles.sidebarLink} href="#">
+              <Image
+                className={styles.sidebarImg}
+                src="/img/playlist03.png"
+                alt="Инди заряд"
+                width={250}
+                height={150}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
